66a0a3e0aed7d919d40fec18e3bce070cf5a7e4e
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2014-02-25 17:21:33 +0700

    Update version (2014-02-25)

--- lib/sh_functions.inc.php
+++ lib/sh_functions.inc.php
@@ -1,12 +1,13 @@
 <?
 
-$SH_VERSION = "2010-10-21";
+$SH_VERSION = "2014-02-25";
 
 require_once dirname(__FILE__).'/sh_translation.inc.php';
 #load_lang_file("grid.yaml");
 #load_lang_file("activerecord.yaml");
 
 require_once dirname(__FILE__).'/sh_utils.inc.php';
+require_once dirname(__FILE__).'/sh_utils_id.inc.php';
 require_once dirname(__FILE__).'/sh_activerecord.inc.php';
 require_once dirname(__FILE__).'/sh_grid.inc.php';
 
